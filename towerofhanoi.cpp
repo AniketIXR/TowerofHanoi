4bc20b35ac8687f25f82ebba7fd8b7310f3506e0
Aniket Roy <40049375+AniketIXR@users.noreply.github.com>
2022-02-21 16:17:41 +0530

    Add files via upload

---
+++ towerofhanoi.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+void toh(int x,int y,int z,int n)
+{
+    if(n>=1)
+    {
+    toh(x,z,y,n-1);
+    cout<<"Move disk "<<n<<" from "<<x<<" to "<<z<<"\n";
+     toh(y,x,z,n-1);
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number of disk: ";
+    cin>>n;
+    toh(1,2,3,5);
+    return 0;
+}
